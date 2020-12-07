2c1783c523716147805972bcdab24f62b0dca413
Stephen Cheung <hc.stephen.cheung@gmail.com>
2020-12-07 01:05:24 -0800

    Initial storybook setup

---
+++ src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Card = () => (
+    <div>Hello World</div>
+)
+
+export default Card;
---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Card } from "./Card";
+
+export default {
+    title: "Demo/Card",
+    component: Card
+};
+
+export const Basic = () => <Card />
